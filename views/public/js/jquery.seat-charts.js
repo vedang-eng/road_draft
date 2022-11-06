d6943e24d6e681f546d9d67309ac88c074a7ba57
Vedang Surnis <vedang.surnis@gmail.com>
2022-11-06 20:39:21 +0530

    Final Add(Kaam 25)

--- views/public/js/jquery.seat-charts.js
+++ views/public/js/jquery.seat-charts.js
@@ -5,16 +5,16 @@
  * Copyright 2013, 2014 Mateusz Markowski
  * Released under the MIT license
  */
-let mybutton = document.getElementById("myBtn");
-window.onscroll = function () { scrollFunction() };
+// let mybutton = document.getElementById("myBtn");
+// window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         mybutton.style.display = "block";
+//     } else {
+//         mybutton.style.display = "none";
+//     }
+// }
 
 (function($) {
 		
